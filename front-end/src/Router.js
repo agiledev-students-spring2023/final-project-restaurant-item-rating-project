0e7e8ed5edf48af4d9ed2070f2fdbf686ade3323
janakbalar <jb7357@nyu.edu>
2023-03-12 15:26:13 -0400

    dishreview page

--- front-end/src/Router.js
+++ front-end/src/Router.js
@@ -1,18 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Template } from './components/Template';
+// import { Template } from './components/Template';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-
+import { DishReview } from './components/DishReview';
 function MyRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} /> 
-          <Route 
+          {/* <Route 
             path="/template"
             element={<Template />} 
+          />  */}
+          <Route 
+            path="/dishreview"
+            element={<DishReview />} 
           /> 
 
         </Route>
---
+++ front-end/src/components/dishreview.js
@@ -0,0 +1,29 @@
+import { Box, Typography } from "@mui/material";
+
+function Title(props) {
+  return (
+    <Typography
+    variant="h1"
+  >
+    {props.title}
+  </Typography>
+  )
+}
+
+
+export function DishReview() {
+
+  // const bodyText = "This is some body text!";
+
+  return (
+    <Box>
+      <Title title="Leave a review" />
+      <Typography
+        variant="body1"
+      >
+        {/* {bodyText} */}
+        
+      </Typography>
+    </Box>
+  );
+}
