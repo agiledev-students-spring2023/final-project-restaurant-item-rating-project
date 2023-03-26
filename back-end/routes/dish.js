9499aa55a66e23a0489ded95409bc88ea0f559fd
ASK212 <ask780@nyu.edu>
2023-03-26 10:18:26 -0400

    Adding Dish.js
    Creating Dish.js

--- back-end/routes/dish.js
+++ back-end/routes/dish.js
@@ -12,3 +12,43 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     res.send('POST request to the homepage')
 })
+
+/*
+// GET route for getting a specific dish
+router.get('/dishes/:id', (req, res) => {
+    const id = req.params.id;
+    // You can use the `id` parameter to look up the dish in your database or in-memory store
+    const dish = findDishById(id);
+    if (!dish) {
+      return res.status(404).json({ error: `Dish with ID ${id} not found` });
+    }
+    res.json(dish);
+  });
+  
+  // POST route for creating a new dish
+  router.post('/dishes', (req, res) => {
+    const dish = req.body;
+    // You can use the `dish` object to create a new dish in your database or in-memory store
+    createDish(dish);
+    res.status(201).json(dish);
+  });
+  
+  // PUT route for updating an existing dish
+  router.put('/dishes/:id', (req, res) => {
+    const id = req.params.id;
+    const dish = req.body;
+    // You can use the `id` parameter and `dish` object to update an existing dish in your database or in-memory store
+    updateDish(id, dish);
+    res.json(dish);
+  });
+  
+  // DELETE route for deleting a specific dish
+  router.delete('/dishes/:id', (req, res) => {
+    const id = req.params.id;
+    // You can use the `id` parameter to delete the dish from your database or in-memory store
+    deleteDishById(id);
+    res.sendStatus(204);
+  });
+  
+  module.exports = router;
+  */
