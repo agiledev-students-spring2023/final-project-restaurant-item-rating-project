9733be7b965f5dce139ca9ba81371b74b6a971a5
crb623 <crb623@nyu.edu>
2023-03-31 19:15:57 -0400

    home page looks like figma

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,8 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import { Button, Box, Typography, Container, TextField} from "@mui/material";
 import { useEffect, useState } from "react";
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
+import { useNavigate } from 'react-router-dom';
+
 
 function suggestionsMapping (favorite) {
   return (
@@ -19,16 +21,17 @@ function suggestionsMapping (favorite) {
         component="img"
         height="200"
         sx={{ 
-          maxWidth:"200px",
+          maxWidth:"100px",
           display:'flex', 
         }}
         src={"https://picsum.photos/200"}
         title={favorite.title}
       />
     </Box>
+
       <CardContent
         sx={{
-          maxWidth:200
+          maxWidth:100
         }}
       >
         <Typography 
@@ -39,13 +42,19 @@ function suggestionsMapping (favorite) {
           {favorite.title.split(" ").slice(0,4).join(" ")}
         </Typography>
       </CardContent>
+
     </Card>
+
+    
   );
 };
 
 export function Home() {
 
+
   const [suggestions, setSuggestions] = useState([]);
+
+  const navigate = useNavigate();
 
   // mocked api with mockaroo
   const apiUrl =  "https://my.api.mockaroo.com/suggestions?key=07b3ef90";
@@ -53,7 +62,7 @@ export function Home() {
     fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      setSuggestions(data.slice(0,3));
+      setSuggestions(data.slice(0,2));
     });
 
   }, []
@@ -61,18 +70,53 @@ export function Home() {
 
   // this is what gets rendered in the React DOM. Must be one element at the top level
   return (
-    <Box>      
-      <Box
-        sx={{ 
-          margin: "auto 4% auto 4%",
-          display: 'flex',
-          justifyContent: 'space-between'
-        }}
-      >
-        {suggestions.map(suggestionsMapping)}
+
+    <Container>
+
+      <TextField  type="search" id="search" label="Search" sx={{ width: 380 }} />
+
+      <Button onClick={() => navigate('/favorites')}>Your Favorites</Button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Button onClick={() => navigate('/add/restaurant')}>Add Restaurant</Button>
+
+
+      <Typography variant="h6" 
+        component="div" sx={{ flexGrow: 1}}>
+        Recommended for you:
+      </Typography>
+
+
+      <Box>      
+        <Box
+          sx={{ 
+            margin: "auto 4% auto 4%",
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          {suggestions.map(suggestionsMapping)}
+        </Box>
+
       </Box>
 
-    </Box>
+
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+      <Box>      
+        <Box
+          sx={{ 
+            margin: "auto 4% auto 4%",
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          {suggestions.map(suggestionsMapping)}
+        </Box>
+
+      </Box>
+
+      
+    </Container>
   );
 }
-
