69059982dfe6b64e36414339a111e0bcc40cc17c
yl7408 <yl7408@nyu.edu>
2023-04-02 21:48:25 -0400

    suggest test modified

--- back-end/tests/suggestion-route-get-suggestions-success-test.js
+++ back-end/tests/suggestion-route-get-suggestions-success-test.js
@@ -18,23 +18,3 @@ describe('GET /dishes/:cityId', ()=>{
       expect(response.body).toHaveProperty('price', 10);
 })
 })
-describe('PUT /restaurants/:cityId', ()=>{
-    it('updates with JSON containing the restaurant suggestions and returns it', async () => {
-      const updatedsuggest = {name : pizza,
-        price : 10 }
-    const response = await request(app)
-      .put('/restaurant/123').send(updatedsuggest);
-      expect(response.status).toBe(200);
-      expect(response.body).toMatchObject(updatedsuggest);
-})
-})
-describe('PUT /dishes/:cityId', ()=>{
-    it('updates with JSON containing the dish suggestions and returns it', async () => {
-      const updatedsuggest = {name : pizza,
-        price : 10}
-    const response = await request(app)
-      .put('/dish/123').send(updatedsuggest);
-      expect(response.status).toBe(200);
-      expect(response.body).toMatchObject(updatedsuggest);
-    })
-})
