1a1d33540344863b56309be4a8a170b682f2cb6e
yl7408 <yl7408@nyu.edu>
2023-04-03 23:58:37 -0400

    test updated

---
+++ back-end/tests/search-route-get-restaurant-failure-test.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+const app = require('../app');
+describe('GET /search/locationId/restaurantId/:dishId', ()=>{
+    it('failed to respond with search result', async () => {
+    const response = await request(app)
+    .get('/search/123/456/789');
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toHaveProperty('name', 'Pizza');
+      expect(response.body).toHaveProperty('price', 10);
+    })
+})
