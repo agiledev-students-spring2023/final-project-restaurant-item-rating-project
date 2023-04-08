486b5ea966b48f93b770a109c3f5141c4979c663
yl7408 <yl7408@nyu.edu>
2023-04-08 02:13:19 -0400

    suggestion route updated sucessfully with the help of my amazon roomate, argurable changes to sschema

--- back-end/routes/suggestion.js
+++ back-end/routes/suggestion.js
@@ -1,50 +1,40 @@
 const express = require('express');
 //Mongoose required
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const Restaurant = require("./../db");
+const Dish = require("./../db");
+
 mongoose.Promise = global.Promise;
 // this router is used for paths matching "/suggestion"
 const suggestionRouter = express.Router();
-//MongoDB imported
-const suggestionSchema = new mongoose.Schema({
-  Restaurant:{
-    name: String,
-    location: String,
-    dishes: Array},
-    Dish:{
-      name: String
-    }
-   });
 
-const DishModel = mongoose.model('Dish', suggestionSchema);
-const RestaurantModel = mongoose.model('Restaurant', suggestionSchema);
 // handlers
-async function makeRestaurantSuggestions() {
-  const RestaurantFromDb = await RestaurantModel.findById(id).exec();
-  return RestaurantFromDb;
+async function makeRestaurantSuggestions(cityId) {
+  const restaurantFromDb = await Restaurant.findById(cityId).exec();
+  return restaurantFromDb;
 }
-async function makeDishSuggestions() {
-  const DishFromDb = await DishModel.findById(id).exec();
-  return DishFromDb;
+
+async function makeDishSuggestions(cityId) {
+  const dishFromDb = await Dish.findById(cityId).exec();
+  return dishFromDb;
 }
 
 // GET route for dishes
-suggestionRouter.get('/dishes', (req,res)=>{
-    const dishSuggestions = makeDishSuggestions();
+suggestionRouter.get('/dishes', (req, res)=>{
+    const dishSuggestions = makeDishSuggestions(req.params.cityId);
     if (!dishSuggestions) {
-        return res.status(404).json({ error: `Dish suggestions within city ID ${cityId} not found` });
+        return res.status(404).json({ error: `Dish suggestions within city ID ${req.cityId} not found` });
       }
-    res.json(dishSuggestions);
+     res.json(dishSuggestions);
 })
 
 // GET route for restaurants
-suggestionRouter.get('/restaurants', (req,res)=>{
-  restaurantSuggestions = makeRestaurantSuggestions();
+suggestionRouter.get('/restaurants', (req, res)=>{
+  restaurantSuggestions = makeRestaurantSuggestions(req.params.cityId);
     if (!restaurantSuggestions) {
-        return res.status(404).json({ error: `Restaurant suggestions within city ID ${cityId} not found` });
+        return res.status(404).json({ error: `Restaurant suggestions within city ID ${req.cityId} not found` });
       }
     res.json(restaurantSuggestions);
 })
 
-module.exports = RestaurantModel;
-module.exports = DishModel;
+module.exports = suggestionRouter;
