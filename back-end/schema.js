9ff9f6e07a0d727f1dbe75155d8bb85c088f0f06
yl7408 <yl7408@nyu.edu>
2023-04-17 23:28:15 -0400

    validation added to Dishschema, reviewSchema, and RestaurantSchema

--- back-end/schema.js
+++ back-end/schema.js
@@ -1,6 +1,7 @@
 // // this file defines the Models/Schema for objects in our application
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+var assert = require('assert');
 
 // // do not use this code. this is for reference so everyone 
 
@@ -43,13 +44,77 @@ const ReviewSchema = new Schema({
   },
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now, 
+    required: true
   },
   userID: {
     type: Number,
     //I think we need to change this: once we get the users working on back-end
     required: false
   }
+  
+});
+
+
+describe('Review validaiton', () =>{
+    let Review;
+    beforeAll(async () => {
+      await mongoose.connect ('/review', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      Review = mongoose.model('Review', ReviewSchema)
+    });
+    afterAll(async () => {
+      await mongoose.connection.close();
+    }) ;
+
+    it('should be invalid if rating value is missing', ()=>{
+      const badreview1 = new Review({
+        date: {type: Date, default: Date.now},
+        userID: 123,
+      });
+      badreview1.validate((error)=>{
+        assert.equal(error.errors.message, 'Missing rating value');
+      })
+    });
+
+    it('should be invalid if userID is empty', ()=>{
+      const badreview2 = new Restaurant({
+        value: 4, 
+        date: {type: Date, default: Date.now},
+      });
+      badreview2.validate((error)=>{
+        assert.equal(error.errors.message, 'Missing user Id');
+      })
+    });
+    it('should be invalid if the current date is missing', ()=>{
+      const badreview3 = new Restaurant({
+        value:4,
+        userID:123
+      });
+      badreview3.validate((error)=>{
+        assert.equal(error.errors.message, 'Missing current date');
+      })
+  });
+
+  it('should be valid if none of the value, current date, userID is empty, error is throwed otherwise', ()=>{
+        const goodreview1 = new Restaurant({
+          value:4,
+          date: {type: Date, default: Date.now},
+          userID:123
+        });
+        goodreview1.validate(function(error){
+          if(error){
+            console.log(error);
+          }else{
+            counsel.log("Review is valid");
+          }
+  
+    })
+    });
+  
+
 });
 
 // Define the dish schema
@@ -67,12 +132,68 @@ const DishSchema = new Schema({
   //   // TODO: change this...just for testing
   //   default: "https://images.ctfassets.net/o19mhvm9a2cm/3TqdEA20hEleGPCZj2JZJl/297b157fdd3ca108c74f17b1bd5fdfce/Website_RB_HP.png"
   // },
-  // averageRating: Number,
+  averageRating: {type: Number, required: true},
   reviews: [ReviewSchema],
 });
-  
+describe('Dish validaiton', () =>{
+  let Dish;
+  beforeAll(async () => {
+    await mongoose.connect ('/dish', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    Dish = mongoose.model('Dish', DishSchema)
+  });
+  afterAll(async () => {
+    await mongoose.connection.close();l
+  }) ;
+
+  it('should be invalid if dish name is empty', ()=>{
+    const badrestaurant1 = new Dish({
+      averageRating : 3.5,
+      reviews: [{ value:4,
+        date: {type: Date, default: Date.now},
+        userID:123}, { value:3,
+          date: {type: Date, default: Date.now},
+          userID:456}],
+        
+    });
+    badrestaurant1.validate((error)=>{
+      assert.equal(error.errors.message, 'Missing dish name');
+    })
+  });
+
+  it('should be invalid if review is empty', ()=>{
+    const badDish2 = new Dish({
+      name: 'Tacos1',
+    });
+    badDish2.validate((error)=>{
+      assert.equal(error.errors.message, 'Missing reviews');
+    })
+  });
+    it('should be valid if none of the name, reviews and average ratings are empty, throws error otherwise', ()=>{
+      const goodDish1 = new Dish({
+        name: 'Tacos1',
+        averageRating : 3.5,
+        reviews: [{ value:4,
+          date: {type: Date, default: Date.now},
+          userID:123}, { value:3,
+            date: {type: Date, default: Date.now},
+            userID:456}],
+      });
+      goodDish1.validate(function(error){
+            if(error){
+              console.log(error);
+            }else{
+              counsel.log("Restaurant is valid")
+            }
+    
+      })
+  })
+})
 // Define the Restaurant schema
 const RestaurantSchema = new Schema({
+
   name: {
     type: String,
     required: true
@@ -82,7 +203,66 @@ const RestaurantSchema = new Schema({
     required: true
   },
   dishes: [DishSchema],
+
 });
+
+describe('Restaurant validaiton', () =>{
+  let Restaurant;
+  beforeAll(async () => {
+    await mongoose.connect ('/restaurant', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    Restaurant = mongoose.model('Restaurant', RestaurantSchema)
+  });
+  afterAll(async () => {
+    await mongoose.connection.close();l
+  }) ;
+
+  it('should be invalid if name is empty', ()=>{
+    const badrestaurant1 = new Restaurant({
+      location: 'NYC',
+      dishes: [{name: 'tacos1', price: 9.99}, {name: 'tacos2', price: 8.99}],
+    });
+    badrestaurant1.validate((error)=>{
+      assert.equal(error.errors.message, 'Missing restaurant name');
+    })
+  });
+
+  it('should be invalid if location is empty', ()=>{
+    const badrestaurant2 = new Restaurant({
+      name: 'Los tacos',
+      dishes: [{name: 'tacos1', price: 9.99}, {name: 'tacos2', price: 8.99}],
+    });
+    badrestaurant2.validate((error)=>{
+      assert.equal(error.errors.message, 'Missing restaurant location');
+    })
+  }); 
+  it('should be invalid if dish list is empty', ()=>{
+    const badrestaurant2 = new Restaurant({
+      name: 'Los tacos',
+      location: 'NYC',
+    });
+    badrestaurant2.validate((error)=>{
+      assert.equal(error.errors.message, 'Missing dish list');
+    })
+  });
+    it('should be valid if none of the name, location and dishes are empty, error is throwed otherwise', ()=>{
+      const goodrestaurant1 = new Restaurant({
+        name: 'Los tacos',
+        location: 'NYC',
+        dishes: [{name: 'tacos1', price: 9.99}, {name: 'tacos2', price: 8.99}],
+      });
+      goodrestaurant1.validate(function(error){
+            if(error){
+              console.log(error);
+            }else{
+              counsel.log("Restaurant is valid")
+            }
+    
+      })
+  })
+})
 
 RestaurantSchema.index({'$**': 'text'});
 
