b43664f987851cb7a0cfd25b35ccb229c174b4e9
yl7408 <yl7408@nyu.edu>
2023-04-20 15:52:39 -0400

    Reviewschema reviewed

--- back-end/schema.js
+++ back-end/schema.js
@@ -44,6 +44,20 @@ const RegisterSchema = new Schema({
   }
 });
 
+const Register = mongoose.model('Register',RegisterSchema);
+const goodregister1 = new Register({
+  email:'test@example.com',
+  password:'password123'
+});
+goodregister1.validateSync(function(error){
+  if(error){
+    console.log(error);
+  }else{
+    counsel.log("Review is valid");
+  }
+
+});
+
 // Define the review schema
 
 const ReviewSchema = new Schema({
@@ -62,8 +76,7 @@ const ReviewSchema = new Schema({
   },
   userID: {
     type: Number,
-    //I think we need to change this: once we get the users working on back-end
-    required: false
+    required: true
   }
   
 });
@@ -75,7 +88,7 @@ const Review = mongoose.model('Review', ReviewSchema)
   const goodreview1 = new Review({
           value:4,
           date: {type: Date, default: Date.now},
-          userID:123
+          userID: 123
         });
         goodreview1.validateSync(function(error){
           if(error){
@@ -88,7 +101,7 @@ const Review = mongoose.model('Review', ReviewSchema)
 // Define the dish schema
 const DishSchema = new Schema({
   name: {
-    type: String,
+    type: Number,
     required: true
   },
   // TODO: implement this stuff..uploads and method on model to calculate,
@@ -109,9 +122,9 @@ const DishSchema = new Schema({
         averageRating : 3.5,
         reviews: [{ value:4,
           date: {type: Date, default: Date.now},
-          userID:123}, { value:3,
+          userID: 123}, { value:3,
             date: {type: Date, default: Date.now},
-            userID:456}],
+            userID: 456}],
       });
       goodDish1.validateSync(function(error){
             if(error){
@@ -136,25 +149,25 @@ const RestaurantSchema = new Schema({
 
 });
 
-// const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
-// const goodrestaurant1 = new Restaurant({
-//         name: 'Los tacos',
-//         location: 'NYC',
-//         dishes: [{name: 'tacos1', price: 9.99}, {name: 'tacos2', price: 8.99}],
-//       });
-//       goodrestaurant1.validateSync(function(error){
-//             if(error){
-//               console.log(error);
-//             }else{
-//               counsel.log("Restaurant is valid")
-//             };
+ const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+ const goodrestaurant1 = new Restaurant({
+        name: 'Los tacos',
+        location: 'NYC',
+        dishes: [{name: 'tacos1', price: 9.99}, {name: 'tacos2', price: 8.99}],
+     });
+     goodrestaurant1.validateSync(function(error){
+          if(error){
+               console.log(error);
+            }else{
+              counsel.log("Restaurant is valid")
+            };
     
-//       });
+      });
 
-// RestaurantSchema.index({'$**': 'text'});
+RestaurantSchema.index({'$**': 'text'});
 
 
-module.exports = {RestaurantSchema,RegisterSchema};
+module.exports = {RestaurantSchema,RegisterSchema, ReviewSchema, DishSchema};
 // module.exports = RegisterSchema;
 
 // module.exports = DishSchema;
