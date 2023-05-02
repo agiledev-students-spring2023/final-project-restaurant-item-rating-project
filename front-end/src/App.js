8cbf3cb5866f34b13f4ad523a68402d23e0e09aa
crb623 <crb623@nyu.edu>
2023-05-02 13:15:34 -0400

    app.js cleanedup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import { MyRoutes } from './Router';
-
-
+//import all routes 
 
 function App() {
+
+  //calling upon routes from following components:
+/*  './components/Layout';
+'./components/Home'
+'./components/DishReview'
+'./components/AddDish'
+'./components/AddRestaurant'
+'./components/DishDetail'
+  import { Search } from './components/Search';
+  import { About } from './components/About';
+  import { RestaurantDetail } from './components/RestaurantDetail';
+  import { Login } from './components/Login';
+  import { Register } from './components/Register';
+  import { Profile } from './components/Profile';
+  import {Favorites} from './components/Favorites'; */
 
   return (
     <MyRoutes />
